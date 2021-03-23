4c52ced0472a0d6d9ec04a58f9281824d8ae7f14
Kimberly <45631466+asdkimberly@users.noreply.github.com>
2021-03-23 03:20:25 -0600

    cambio de "gtf" por "gradloss"

--- gradloss.m
+++ gradloss.m
@@ -1,4 +1,4 @@
 %% Gradient of the loss function
-function gtf=gradloss(theta,X,Y)
-  gtf=(XX'*(XX*theta'-Y*ones(1,rows(theta))))';
+function gradloss =gtf(theta,X,Y)
+  gradloss=(XX'*(XX*theta'-Y*ones(1,rows(theta))))';
 endfunction;
