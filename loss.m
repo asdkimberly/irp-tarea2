c72e338f08193d7b3a782243bbe7de5b6e6025b7
Kimberly <45631466+asdkimberly@users.noreply.github.com>
2021-03-23 12:50:10 -0600

    Se revierte el cambio del nombre de función (loss-tf)

--- loss.m
+++ loss.m
@@ -1,9 +1,9 @@
 %% Loss function - utilizando codigo del curso con ligeras modificaciones propias
 %% Objective function of the parameters theta using the data and labels
 
-function loss=tf(theta,XX,Y)
+function tf=loss(theta,XX,Y)
   ## Calculando el residuo para las tuplas
   R=(hypothesis(XX,theta')-Y*ones(1,rows(theta)));
   ## Sumando el producto punto de los residuos en un escalar
-  loss=0.5*sum(R.*R,1)';
+  tf=0.5*sum(R.*R,1)';
 endfunction;
