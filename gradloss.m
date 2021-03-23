4d598a7fcdfffa5dc7b1d8470d19561b7b2c015b
Kimberly <45631466+asdkimberly@users.noreply.github.com>
2021-03-23 03:10:19 -0600

    cambio de nombre 'gtf' en 'res'

--- gradloss.m
+++ gradloss.m
@@ -1,9 +1,4 @@
 %% Gradient of the loss function
-function res=gradloss(theta,X,Y)
-  order = columns(theta)-1;
-
-  ## ################################################################
-  ## Your code in here!!!
-
-  res = rand(size(theta)); ## dummy result
+function gtf=gradloss(theta,X,Y)
+  gtf=(XX'*(XX*theta'-Y*ones(1,rows(theta))))';
 endfunction;
