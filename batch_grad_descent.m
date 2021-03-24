62dccebd9906cc94f1c312c8d2abc33e884cc8b3
Andyjzjz <64620118+Andyjzjz@users.noreply.github.com>
2021-03-23 22:03:29 -0600

    Update batch_grad_descent.m
    x

--- batch_grad_descent.m
+++ batch_grad_descent.m
@@ -1,9 +1,9 @@
 #!/usr/bin/octave-cli --persist
 
 ## (C) 2020 Pablo Alvarado
-## EL5852 Introducción al Reconocimiento de Patrones
-## Escuela de Ingeniería Electrónica
-## Tecnológico de Costa Rica
+## EL5852 Introducciû˚n al Reconocimiento de Patrones
+## Escuela de IngenierûÙa Electrû˚nica
+## Tecnolû˚gico de Costa Rica
 
 # --------------------------------------------------------------------
 # Linear regression, showing the contours and the estimated line.
@@ -15,23 +15,58 @@
 
 pkg load optim;
 
+%theta0=[0.7 -0.25];
+
+order = 1;
+
+
+
 ## Data stored each sample in a row, where the last row is the label
-D=load("escazu.dat");
+D=load("escazu41.dat");
 
 ## Construct the design matrix with a 1's column and the original area
 Xo=[ones(rows(D),1),D(:,1)];
 
-## normalizer_type="normal";
-normalizer_type="minmax";
+normalizer_type="normal";
 
 ## Normalize the data
 nx = normalizer(normalizer_type);
-X = nx.fit_transform(Xo);
+x = nx.fit_transform(Xo);
+
+
+
+if (order<1)
+    error("El punto inicial de theta0 debe tener al menos 2 dimensiones");
+  %%Creamos las matrices de diseño completas; autores
+  %para orden 1
+elseif (order==1)
+    XX=[x];
+    
+  %para orden 2  
+elseif (order==2)
+    %XX=[ones(rows(x),1) x x.^2 x(:,1).*x(:,2) x(:,1).*x(:,3) x(:,2).*x(:,3)]; %Para 3D
+    XX=[x x(:,2).^2];
+    
+  %para orden 3  
+elseif (order==3)
+    %XX=[ones(rows(x),1) x x.^2 x(:,1).*x(:,2) x(:,1).*x(:,3) x(:,2).*x(:,3) x.^3 (x(:,1).^2).*x(:,2) (x(:,1).^2).*x(:,3) x(:,1).*(x(:,2).^2) x(:,1).*(x(:,3).^2) (x(:,2).^2).*x(:,3) x(:,2).*(x(:,3).^2) x(:,1).*x(:,2).*x(:,3)];
+    XX=[x x(:,2).^2 x(:,2).^3];
+    
+  %para orden 4  
+elseif (order==4)
+    %XX=[ones(rows(x),1) x x.^2 x(:,1).*x(:,2) x(:,1).*x(:,3) x(:,2).*x(:,3) x.^3 (x(:,1).^2).*x(:,2) (x(:,1).^2).*x(:,3) x(:,1).*(x(:,2).^2) x(:,1).*(x(:,3).^2) (x(:,2).^2).*x(:,3) x(:,2).*(x(:,3).^2) x(:,1).*x(:,2).*x(:,3) x.^4 (x(:,1).^3).*x(:,2) (x(:,1).^3).*x(:,3) x(:,1).*(x(:,2).^3) x(:,1).*(x(:,3).^3) (x(:,2).^3).*x(:,3) x(:,2).*(x(:,3).^3) (x(:,1).^2).*(x(:,2).^2) (x(:,1).^2).*(x(:,3).^2) (x(:,2).^2).*(x(:,3).^2) (x(:,1).^2).*x(:,2).*x(:,3) x(:,1).*(x(:,2).^2).*x(:,3) x(:,1).*x(:,2).*(x(:,3).^2)];
+    XX=[x x(:,2).^2 x(:,2).^3 x(:,2).^4];
+    
+elseif (order>4)
+    error("El límite de orden es de 4");
+endif
+
 
 ## The outputs vector with the original data
 Yo=D(:,4);
 ny = normalizer(normalizer_type);
 Y = ny.fit_transform(Yo);
+
 
 ## Limits for plot of regressed lines
 minArea = min(Xo(:,2));
@@ -67,53 +102,55 @@ function res=gradJ(theta,X,Y)
   res=(X'*(X*theta'-Y*ones(1,rows(theta))))';
 endfunction;
 
-## The following segment shows the error surface,
-## which is possible only because in this case
-## we have a 2D theta space [theta0 theta1] and
-## nothing else
-
-## Select the grid to plot the contours and gradients
-th0=-1:0.05:1;   ## Value range for theta0
-th1=-0.5:0.05:2; ## Value range for theta1
-[tt0,tt1] = meshgrid(th0,th1);  ## The complete grid
-contwnd = [th0(1) th0(end) th1(1) th1(end)];
-
-theta=[tt0(:) tt1(:)]; ## All theta value pairs in rows
-jj=reshape(J(theta,X,Y),size(tt0)); ## J values for each pair
-
-## Precompute the gradient for the chosen grid
-g=gradJ(theta,X,Y);
-gjx=reshape(g(:,1),size(tt0));
-gjy=reshape(g(:,2),size(tt1));
-
-## Show the J surface
-figure(3,"name","J");
-hold off;
-surf(tt0,tt1,jj);
-xlabel('{\theta_0}');
-ylabel('{\theta_1}');
-
-## Plot the contours in 2D
-figure(1,"name","Contours");
-hold off;
-
-## Plot the ellipses of the error surface
-contour(tt0,tt1,jj);
-hold on;
-## and also its the gradient
-quiver(tt0,tt1,gjx,gjy,0.7);
-xlabel("theta_0");
-ylabel("theta_1");
-axis(contwnd);
-daspect([1,1]);
 
 
-## Learning rate
-alpha = 0.05;
+%%Debemos saber la cantidad de thetas0 con las que contamos para
+%definir el punto inicial del descenso de gradiente
 
-## Do the learning:
-
-while(1)
+if (order==1) %espacio de error con un espacio de theta de 2D
+ th0=-1:0.05:1;   ## Value range for theta0
+ th1=-0.5:0.05:2; ## Value range for theta1
+ 
+ [tt0,tt1] = meshgrid(th0,th1);  ## The complete grid
+ contwnd = [th0(1) th0(end) th1(1) th1(end)];
+ 
+ theta=[tt0(:) tt1(:)]; ## All theta value pairs in rows
+ jj=reshape(J(theta,XX,Y),size(tt0)); ## J values for each pair
+ 
+ ## Precompute the gradient for the chosen grid
+ g=gradJ(theta,XX,Y);%% !!OJO!! esta funcion de gradiente así la 
+ %J del método anterior  J(theta,XX,Y) son las funciones gradloss y loss
+ %respectivamente
+ gjx=reshape(g(:,1),size(tt0));
+ gjy=reshape(g(:,2),size(tt1));
+ 
+ ## Show the J surface
+ figure(3,"name","J");
+ hold off;
+ surf(tt0,tt1,jj);
+ xlabel('{\theta_0}');
+ ylabel('{\theta_1}');
+ 
+ ## Plot the contours in 2D
+ figure(1,"name","Contours");
+ hold off;
+ 
+ ## Plot the ellipses of the error surface
+ contour(tt0,tt1,jj);
+ hold on;
+ ## and also its the gradient
+ quiver(tt0,tt1,gjx,gjy,0.7);
+ xlabel("theta_0");
+ ylabel("theta_1");
+ axis(contwnd);
+ daspect([1,1]);
+ 
+ ## Learning rate
+ alpha = 0.05;   %%OJO ESTO ES UN PARAMETRO EN LA FUNCION DESCENTPOLY
+ 
+ %%EMPIEZA A GENERAR EL DESCENSO DE GRADIENTE, do the learning
+ 
+ while(1)
   hold on;
  
   printf("Click on countours to set a starting point\n");
@@ -124,8 +161,8 @@ while(1)
 
   ## Wait for a mouse click and get the point (t0,t1) in the plot coordinate sys.
   [t0,t1,buttons] = ginput(1);
-  t=[t0,t1];
-  gt=gradJ(t,X,Y);
+  t=[t0,t1];%%ESTOS SON LOS ELEMETOS DE THETA0
+  gt=gradJ(t,XX,Y);
 
   ## Clean the previous plot 
   hold off;
@@ -141,7 +178,7 @@ while(1)
   ylabel('{\theta_1}');
  
   ## Print some information on the clicked starting point
-  printf("J(%g,%g)=%g\n",t0,t1,J(t,X,Y));
+  printf("J(%g,%g)=%g\n",t0,t1,J(t,XX,Y));
   printf("  GradJ(%g,%g)=[%g,%g]\n",t0,t1,gt(1),gt(2));
   fflush(stdout);
 
@@ -157,7 +194,7 @@ while(1)
 
   for i=[1:100] # max 100 iterations
     tc = ts(end,:); # Current position 
-    gn = gradJ(tc,X,Y);  # Gradient at current position
+    gn = gradJ(tc,XX,Y);  # Gradient at current position
     tn = tc - alpha * gn;# Next position
     ts = [ts;tn];
 
@@ -175,13 +212,13 @@ while(1)
   hold on;
   
   ## We have to de-normalize the normalized estimation
-  nprices = nareas * ts(1,:)';
+  nprices = nareas * ts(1,:)';%LO HACE solo con el elemento de la primera fila de ts
   prices=ny.itransform(nprices);
  
   plot(areas,prices,'k',"linewidth",2);
 
   ## and now with the intermediate versions
-  for (i=[2:rows(ts)])
+  for (i=[2:rows(ts)])%lo hace desde el elemeto de la segunda fila de ts hasta el final de ts
     nprices = nareas * ts(i,:)';
     prices=ny.itransform(nprices); 	
     plot(areas,prices,'r',"linewidth",1);
@@ -190,4 +227,27 @@ while(1)
   plot(areas,prices,'g',"linewidth",3);
 
   axis([minArea maxArea minPrice maxPrice]);  
-endwhile;
+ endwhile;
+
+ 
+elseif (order==2) %espacio de error con un espacio de theta de 3D
+ th0=theta0(1,1);
+ th1=theta0(1,2);
+ th2=theta0(1,3);
+ 
+ 
+
+elseif (order==3) %espacio de error con un espacio de theta de 4D
+ th0=theta0(1,1);
+ th1=theta0(1,2);
+ th2=theta0(1,3);
+ th3=theta0(1,4);
+
+elseif (order==4) %espacio de error con un espacio de theta de 5D
+ th0=theta0(1,1);
+ th1=theta0(1,2);
+ th2=theta0(1,3);
+ th3=theta0(1,4);
+ th4=theta0(1,5);
+endif
+
